133ff48b87f5f042caf7de23d0c6085e0eba1a30
Ree <rahayukartikasari12@gmail.com>
2024-03-29 17:12:50 +0700

    add countdown

--- UTS - GAME JS/script.js
+++ UTS - GAME JS/script.js
@@ -3,7 +3,8 @@ const form = document.getElementById('game-form');
 const nameInput = document.getElementById('name');
 const levelSelect = document.getElementById('level');
 const canvasContainer = document.getElementById('canvas-container');
-const overlayContainer = document.getElementById('canvas-overlay-container');
+const startOverlay = document.getElementById('canvas-overlay-container-start');
+const countdownOverlay = document.getElementById('canvas-overlay-countdown');
 const topPanel =  document.getElementById('top-panel-container')
 const playerName = document.getElementById("player-name")
 const score = document.getElementById("score")
@@ -14,8 +15,9 @@ form.addEventListener('submit', startGame);
 function startGame(event) {
   event.preventDefault();
   canvasContainer.style.display = 'block';
-  overlayContainer.style.display = 'none';
+  startOverlay.style.display = 'none';
   topPanel.style.display = 'grid';
+  showCountdown()
 
   const nameInputValue = nameInput.value;
   const levelSelectValue = levelSelect.value;
@@ -36,207 +38,27 @@ function createImage(src){
   return image
 }
 
-function drawMap(level){
-  let map = []
-  switch(level){
-    case "Easy":
-      map = ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
-            ['|', '', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '7', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', 'p', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '+', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '5', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|'],
-            ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
-    case "Medium":
-      map = ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
-            ['|', '', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '7', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', 'p', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '+', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '5', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|'],
-            ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
-    case "Hard":
-      map = ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
-            ['|', '', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '7', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', 'p', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '+', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|'],
-            ['|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|'],
-            ['|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|'],
-            ['|', '.', 'b', '.', '[', '5', ']', '.', 'b', '.', '|'],
-            ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|'],
-            ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
-  }
-  
-  return map
-}
-
-function drawBoundary(){
-  const map = drawMap()
-  const boundaries = []
-
-  // Push in boundary
-  map.forEach((row, rowIndex) => {
-      row.forEach((symbol, colIndex) => {
-          switch(symbol){
-              case '-':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeHorizontal.png')
-                  }))
-                  break;
-              case '|':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeVertical.png')
-                  }))
-                  break;
-              case '1':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeCorner1.png')
-                  }))
-                  break;
-              case '2':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeCorner2.png')
-                  }))
-                  break;
-              case '3':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeCorner3.png')
-                  }))
-                  break;
-              case '4':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeCorner4.png')
-                  }))
-                  break;
-              case '5':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeConnectorTop.png')
-                  }))
-                  break;
-              case '7':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeConnectorBottom.png')
-                  }))
-                  break;
-              case 'b':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/block.png')
-                  }))
-                  break;
-              case '[':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/capLeft.png')
-                  }))
-                  break;
-              case ']':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/capRight.png')
-                  }))
-                  break;
-              case '^':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/capTop.png')
-                  }))
-                  break;
-              case '+':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/pipeCross.png')
-                  }))
-                  break;
-              case '_':
-                  boundaries.push(new Boundary({
-                      position: {
-                          x: Boundary.width * colIndex,
-                          y: Boundary.height * rowIndex
-                      },
-                      image: createImage('./assets/images/capBottom.png')
-                  }))
-                  break;
-              default:
-                  break;
-          }
-      })
-  })
-
-  return boundaries
+function showCountdown() {
+  countdownOverlay.style.display = 'block';
+  countdownOverlay.textContent = '3';
+  setTimeout(() => {
+    countdownOverlay.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdownOverlay.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdownOverlay.textContent = 'Ready';
+  }, 3000);
+  setTimeout(() => {
+    countdownOverlay.textContent = 'Go!';
+    setTimeout(() => {
+      countdownOverlay.style.display = 'none';
+    }, 1000);
+  }, 4000);
 }
 
 function loadGame(level){
-  drawMap(level)
 
 }
 
-
-//Points
-const FINAL_LEVEL = 10;
-const PILL_POINTS = 10;
-const POWERPILL_POINTS = 50;
-const GHOST_POINTS = 100;
